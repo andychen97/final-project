aabcca25e5fbfa3db6508480784d68a7a990bd24
Andy Chen <chen.andy18@gmail.com>
2022-07-27 04:07:00 +0000

    in progress.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const path = require('path');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
+const fetch = require('node-fetch');
 
 const app = express();
 const publicPath = path.join(__dirname, 'public');
@@ -15,17 +16,20 @@ if (process.env.NODE_ENV === 'development') {
 // const baseUrl = process.env.API_BASE_URL;
 // const token = process.env.BEARER_TOKEN;
 
-// app.get('api/search', (req, res, next) => {
-//   const queryParams = req.query;
-//   return fetch(`${baseUrl}/${path}/${queryParams}`, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       Origin: 'localhost',
-//       withCredentials: true
-//     }
-//   }
+app.get('api/search', (req, res, next) => {
+  // const queryParams = req.query;
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Authorization', 'mds9ZwJ2nyEEmEDEHWa_Ti-0HDOvqYjhYlqeM3fSGJnCQIyMHPFAbs0vgBpwDNKO0-Ee_k-hdLfNYOi46vXb_RVqvHT63rYS9ijKnjoN4NLczvkBDVKcYpq2MTXbYnYx');
 
-// });
+  fetch('https://api.yelp.com/v3/businesses/search?term=delis&location=irvine', {
+    method: 'GET',
+    headers: myHeaders
+  })
+    .then(res => res.send());
+  // .then(data => console.log(data))
+  // .catch(err => console.log('err', err));
+});
 
 app.use(errorMiddleware);
 
