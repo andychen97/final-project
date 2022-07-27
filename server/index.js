aee5b07f46e7e03758b2c35e45500e2219a113e2
Andy Chen <chen.andy18@gmail.com>
2022-07-27 09:20:48 +0000

    feature 1 95% complete.

--- server/index.js
+++ server/index.js
@@ -12,19 +12,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(express.static(publicPath));
 }
 
-// const baseUrl = process.env.API_BASE_URL;
+const baseUrl = process.env.API_BASE_URL;
 const token = process.env.BEARER_TOKEN;
 
 app.use(express.json({
   type: ['application/json', 'text/plain']
 }));
-// app.use(express.json());
 app.post('/api/search', (req, res, next) => {
   const { keyword, location } = req.body;
-  // const myHeaders = new Object();
-  // myHeaders.append('Authorization', `Bearer ${token}`);
-  // myHeaders.append('Accept', 'application/json');
-  // myHeaders.append('Content-Type', 'application/json');
 
   const reqs = {
     method: 'GET',
@@ -35,7 +30,7 @@ app.post('/api/search', (req, res, next) => {
     }
   };
 
-  fetch(`https://api.yelp.com/v3/businesses/search?term=${keyword}&location=${location}`, reqs)
+  fetch(`${baseUrl}/businesses/search?term=${keyword}&location=${location}`, reqs)
     .then(result => result.json())
     .then(data => res.json(data))
     .then(resp => res.status(200))
