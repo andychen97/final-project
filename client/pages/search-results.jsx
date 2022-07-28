4065f9c4177b2b10b0476f18debd406750e15241
Andy Chen <chen.andy18@gmail.com>
2022-07-28 06:17:11 +0000

    in progress.

---
+++ client/pages/search-results.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogoHeader from '../components/header';
+import ResultBox from '../components/result-box';
+
+export default function Home(props) {
+  return (
+    <div>
+      <LogoHeader />
+      <ResultBox />
+    </div>
+  );
+}
