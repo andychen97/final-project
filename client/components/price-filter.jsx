d97e2d92edbf84a2cf5164fa5c68b9c0548746ac
Andy Chen <chen.andy18@gmail.com>
2022-08-01 14:35:47 +0000

    accidentally deleted a file.

---
+++ client/components/price-filter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class PriceFilter extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <button className='filter-prices'>$</button>
+        <button className='filter-prices'>$$</button>
+        <button className='filter-prices'>$$$</button>
+        <button className='filter-prices'>$$$$</button>
+      </div>
+    );
+  }
+}
