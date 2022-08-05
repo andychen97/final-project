7ccf0591e53bba2fe4a91439f68ba39c872d94a2
Andy Chen <chen.andy18@gmail.com>
2022-08-05 04:31:05 +0000

    profile in progress.

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -4,8 +4,24 @@ import AppContext from '../lib/app-context';
 export default class Profile extends React.Component {
   render() {
     const { user } = this.context;
+    const firstName = user.firstName[0].toUpperCase() + user.firstName.slice(1, user.firstName.length);
+    const lastName = user.lastName[0].toUpperCase() + user.lastName.slice(1, user.lastName.length);
     return (
-      <button>{user.firstName}</button>
+      <div className="row">
+        <div className="card">
+          <div className='picture-header'>
+            <div className="picture-frame">
+              <img className='profile-card-img'src="https://randomuser.me/api/portraits/men/3.jpg" alt="profile-picture" />
+            </div>
+            <h1 className='profile-card-name'>{firstName} {lastName}</h1>
+            <p className='profile-review-count'>0 Reviews</p>
+          </div>
+          <div>
+            <h3 className='profile-recent-reviews'>Recent Reviews</h3>
+            <hr className='grey-line'/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
